bb30d1d185d92c0386740b575caafd23480b5b07
manuel-morales-orbidi <manuel.morales@orbidi.com>
2025-02-07 10:50:31 -0500

    add lambdatest integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,15 +20,16 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+
+  //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
     //baseURL: 'https://blanicconsulting.com/',
-    baseURL: process.env.BASE_URL || 'https://www.jorgearturobelliniserrano.com/',
+    //baseURL: process.env.BASE_URL || 'https://www.jorgearturobelliniserrano.com/',
     //baseURL: 'https://www.vitalilaser.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -42,6 +43,19 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
+    {
+      name: 'LambdaTest-Chrome',
+      use: {
+        browserName: 'chromium',
+        headless: true,
+        baseURL: process.env.BASE_URL || 'https://blanicconsulting.com/',
+        viewport: { width: 1280, height: 720 },
+        screenshot: 'on',
+        video: 'on',
+        trace: 'on',
+      },
+    },
+ 
     /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -72,7 +86,8 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
+  workers: 2,
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
